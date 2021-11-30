3acfe27e31e8892ec8e3ec6f20d972aab65838c5
TibTus <tibtus@gmail.com>
2021-11-30 20:07:46 +0200

    #project_add_mask_add_mod_5

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
+
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -11,7 +15,10 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
-
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 
     
 
