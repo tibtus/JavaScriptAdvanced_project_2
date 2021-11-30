f5d6718249d1b2db9b014f4544124fb7823a30b0
TibTus <tibtus@gmail.com>
2021-11-30 21:58:57 +0200

    #project_add_showMoreStyles(getpost_service)_mod_6

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,3 +1,5 @@
+import {postData} from "../services/requests";
+
 const forms = () => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'),
@@ -17,14 +19,7 @@ const forms = () => {
         question: 'assets/question.php'
     };
 
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
 
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
