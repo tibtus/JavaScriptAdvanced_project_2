c1354dd0a1e4203cd3eb93907503666d20611a5c
TibTus <tibtus@gmail.com>
2021-12-03 13:57:21 +0200

    #project_add_accordion_mod_10_job

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 
 
@@ -25,6 +26,6 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
     pictureSize('.sizes-block');
-    
+    accordion('.accordion-heading');
 
 });
